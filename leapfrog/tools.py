17afc74487315e48a134559348b7c776c1439a25
Alex Shadley <shadleyalex@gmail.com>
2018-07-31 23:17:16 -0500

    adds new_transfer_code()

--- leapfrog/tools.py
+++ leapfrog/tools.py
@@ -1,4 +1,6 @@
 from json import JSONEncoder
+from random import random
+import math
 
 
 class ModelEncoder(JSONEncoder):
@@ -12,3 +14,28 @@ class ModelEncoder(JSONEncoder):
             return obj.serializeable()
         except AttributeError:
             return JSONEncoder.default(self, obj)
+
+
+def new_transfer_code(l):
+    """
+    Creates a code used to authenticate transfers, consisting of upper- and lowercase alphanumerics.  Technically this
+    doesn't guarantee uniqueness between codes, but at l = 30 the chance of two codes colliding is 1 in 5.9 x 10^53.  If
+    this fails, I'm moving to Vegas.
+
+    Args:
+        l: code length
+
+    Returns:
+        code: transfer code
+
+    """
+    code = ''
+    for x in range(l):
+        r = math.floor(random() * 62)  # 26 Uppercase + 26 Lowercase + 10 digits
+        if r < 26:
+            code += chr(r + 65)
+        elif r < 52:
+            code += chr(r + 71)
+        else:
+            code += chr(r - 4)
+    return code
